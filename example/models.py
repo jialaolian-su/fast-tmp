5e7b48994c099bf2cd93bf2f50be49670f7a9400
Chise1 <chise123@live.com>
2020-12-06 13:08:44 +0800

    找到序列化器没有正确序列化外键和多对多字段和映射字段的bug。

--- example/models.py
+++ example/models.py
@@ -7,18 +7,29 @@
 @Software: PyCharm
 @info    :
 """
+from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
+
 from tortoise.models import Model
 from tortoise import fields
 
 
 class Author(Model):
+    id = fields.IntField(pk=True)
     name = fields.CharField(max_length=32)
+
     books: fields.ReverseRelation["Book"]
+
+    class Meta:
+        ordering = ["name"]
 
 
 class Book(Model):
+    id = fields.IntField(pk=True)
     name = fields.CharField(max_length=32)
     author: fields.ForeignKeyRelation[Author] = fields.ForeignKeyField("models.Author", related_name="books")
+
+    class Meta:
+        ordering = ["name"]
 
 
 class Publish(Model):
@@ -49,3 +60,67 @@ class Principal(Model):
     school: fields.OneToOneRelation[School] = fields.OneToOneField(
         "models.School", on_delete=fields.CASCADE, related_name="principal", to_field="id"
     )
+
+
+class Tournament(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    events: fields.ReverseRelation["Event"]
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Event(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    tournament: fields.ForeignKeyNullableRelation[Tournament] = fields.ForeignKeyField(
+        "models.Tournament", related_name="events", null=True
+    )
+    participants: fields.ManyToManyRelation["Team"] = fields.ManyToManyField(
+        "models.Team", related_name="events", through="event_team"
+    )
+    address: fields.OneToOneNullableRelation["Address"]
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Address(Model):
+    city = fields.CharField(max_length=64)
+    street = fields.CharField(max_length=128)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    event: fields.OneToOneRelation[Event] = fields.OneToOneField(
+        "models.Event", on_delete=fields.CASCADE, related_name="address", pk=True
+    )
+
+    class Meta:
+        ordering = ["city"]
+
+
+class Team(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    events: fields.ManyToManyRelation[Event]
+
+    class Meta:
+        ordering = ["name"]
+book = pydantic_model_creator(Book)
+Author1 = pydantic_model_creator(Author)
+Author2 = pydantic_model_creator(Author, name='Author2', exclude_readonly=False)
+BOOK1 = pydantic_model_creator(Book, exclude_readonly=True, allow_cycles=True, name='BOOK1')
+BOOK2 = pydantic_model_creator(Book, name='BOOK2')
+
+Event_Pydantic = pydantic_model_creator(Event)
+Event_Pydantic_List = pydantic_queryset_creator(Event)
+Tournament_Pydantic = pydantic_model_creator(Tournament)
+Team_Pydantic = pydantic_model_creator(Team)
+
+
+print(1)
---
+++ t.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : t.py
+@Time    : 2020/12/3 23:46
+@Author  : chise
+@Email   : chise123@live.com
+@Software: PyCharm
+@info    :metaclass学习
+"""
+
+class MyMeta(type):
+    def __init__(self,name,bases,dic):
+        super().__init__(name,bases,dic)
+        print("MyMeta.__init__")
+        print(self.__name__)
+        print(dic)
+        print(self.yaml_tag)
+
+    def __new__(cls, *args, **kwargs):
+        print("MyMeta.__new__")
+        print(cls.__name__)
+        return type.__new__(cls,*args,**kwargs)
+    def __call__(cls, *args, **kwargs):
+        print("MyMeta.__call__")
+        obj=cls.__new__(cls)
+        cls.__init__(cls,*args,**kwargs)
+        return obj
+
+class Foo(metaclass=MyMeta):
+    yaml_tag="!Foo"
+
+    def __init__(self,name):
+        print('Foo.__init__')
+        self.name=name
+    def __new__(cls, *args, **kwargs):
+        print('Foo.__new__')
+        return object.__new__(cls)
+print("-------------------------------------")
+foo=Foo('foo')
