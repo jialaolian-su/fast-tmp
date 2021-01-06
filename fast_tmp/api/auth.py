dd344c88cc5416486f2900ea0be8e81330512882
chise <chise123@live.com>
2021-01-06 09:47:59 +0800

    更新

--- fast_tmp/api/auth.py
+++ fast_tmp/api/auth.py
@@ -9,10 +9,10 @@ from jose import JWTError, jwt
 from pydantic import BaseModel
 from starlette.requests import Request
 
+from fast_tmp.amis_router import AmisRouter
 from fast_tmp.conf import settings
-from fast_tmp.core.amis_router import AmisRouter
 from fast_tmp.core.mixins import AimsListMixin
-from fast_tmp.models import AbstractUser
+from fast_tmp.models import User
 from fast_tmp.schema import UserCreateSchema
 from fast_tmp.templates_app import templates
 from fast_tmp.utils.model import get_model_from_str
@@ -20,7 +20,7 @@ from fast_tmp.utils.model import get_model_from_str
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.EXPIRES_DELTA
-User: Type[AbstractUser] = get_model_from_str(settings.AUTH_USER_MODEL)
+User: User = get_model_from_str(settings.AUTH_USER_MODEL)
 
 
 class AccessTokenInfo(BaseModel):
@@ -48,12 +48,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 auth_router = AmisRouter(prefix="/auth")
 
 
-async def get_user(username: str) -> Optional[AbstractUser]:
+async def get_user(username: str) -> Optional[User]:
     user = await User.filter(username=username).first()
     return user
 
 
-async def authenticate_user(username: str, password: str) -> Optional[AbstractUser]:
+async def authenticate_user(username: str, password: str) -> Optional[User]:
     user = await get_user(username)
     if not user:
         return None
@@ -78,7 +78,7 @@ def create_access_token(data: AccessTokenInfo):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> AbstractUser:
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -92,15 +92,15 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> AbstractUser:
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user: AbstractUser = await get_user(username=token_data.username)
+    user: User = await get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
 
 
 async def get_current_active_user(
-    current_user: AbstractUser = Depends(get_current_user),
-) -> AbstractUser:
+    current_user: User = Depends(get_current_user),
+) -> User:
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
@@ -122,12 +122,12 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 @auth_router.get("/users/me/", response_model=UserSchema)
-async def read_users_me(current_user: AbstractUser = Depends(get_current_active_user)):
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
 
 @auth_router.get("/users/me/items/")
-async def read_own_items(current_user: AbstractUser = Depends(get_current_active_user)):
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
@@ -153,21 +153,6 @@ x.init(auth_router)
 async def template(
     request: Request,
 ):
-    page = {
-        "type": "page",
-        "body": {
-            "type": "crud",
-            "api": "https://houtai.baidu.com/api/sample",
-            "columns": [
-                {"name": "id", "label": "ID"},
-                {"name": "engine", "label": "Rendering engine"},
-                {"name": "browser", "label": "Browser"},
-                {"name": "platform", "label": "Platform(s)"},
-                {"name": "version", "label": "Engine version"},
-                {"name": "grade", "label": "CSS grade"},
-            ],
-        },
-    }
     return templates.TemplateResponse(
         "admin/crud.html", {"request": request, "page": str(json.dumps(page))}
     )
@@ -179,6 +164,18 @@ async def template(
 ):
     return templates.TemplateResponse(
         "admin/t.html",
+        {
+            "request": request,
+        },
+    )
+
+
+@auth_router.get("/index", response_class=HTMLResponse)
+async def index(
+    request: Request,
+):
+    return templates.TemplateResponse(
+        "admin/index.html",
         {
             "request": request,
         },
